0cf170fbe42ef577e3c03f8eaf3ec146c1bf92b3
tamerlane2998 <trieuquangminh29@gmail.com>
2019-09-24 09:02:47 +0700

    Create HocSinh.cs
    Một vài bổ sung nhỏ

---
+++ Bai3_TruongTHPT/Main/HocSinh.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BUS
+{
+    public class HocSinh
+    {
+        public DataTable Show()
+        {
+            string sql = "SELECT hs.MaHS, hs.HovaTen, hs.GT, hs.NgaySinh, hs.DiaChi, hs.PhuHuynh, lop.TenLop FROM dbo.HocSinh hs, dbo.Lop lop where hs.MaLop = lop.MaLop";
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(ConnectDB.getconnect());
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(dt);
+            conn.Close();
+            da.Dispose();
+            return dt;
+        }
+
+        // Chức năng sửa học sinh
+        public void Sua_HS(string MaHS, string HoTen, string GT, string NgaySinh, string DiaChi, string PhuHuynh, string MaLop)
+        {
+            string sql =  "Sua_HS";
+            SqlConnection conn = new SqlConnection(ConnectDB.getconnect());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@HovaTen", HoTen);
+            cmd.Parameters.AddWithValue("@GT", GT);
+            cmd.Parameters.AddWithValue("@NgaySinh", NgaySinh);
+            cmd.Parameters.AddWithValue("@DiaChi", DiaChi);
+            cmd.Parameters.AddWithValue("@PhuHuynh", PhuHuynh);
+            cmd.Parameters.AddWithValue("@MaLop", MaLop);
+            cmd.Parameters.AddWithValue("@MaHS", MaHS);
+
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+        //Chức năng thêm học sinh
+        public void ThemHocSinh(string HovaTen, string GT, DateTime NgaySinh, string DiaChi, string PhuHuynh, string MaLop)
+        {
+            string sql = "ADD_HocSinh";
+            SqlConnection conn = new SqlConnection(ConnectDB.getconnect());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@HovaTen", HovaTen);
+            cmd.Parameters.AddWithValue("@GT", GT);
+            cmd.Parameters.AddWithValue("@NgaySinh", NgaySinh);
+            cmd.Parameters.AddWithValue("@DiaChi", DiaChi);
+            cmd.Parameters.AddWithValue("@PhuHuynh", PhuHuynh);
+            cmd.Parameters.AddWithValue("@MaLop", MaLop);
+
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+
+        // Chức năng Xóa
+        public void Xoa_HS(string MaHS)
+        {
+            string sql = "Xoa_HS";
+            SqlConnection conn = new SqlConnection(ConnectDB.getconnect());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MaHS", MaHS);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+        //lay thong tin lop
+        public DataTable LayThongTinLop()
+        {
+            string sql = "SELECT * FROM dbo.Lop";
+            SqlConnection conn = new SqlConnection(ConnectDB.getconnect());
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
