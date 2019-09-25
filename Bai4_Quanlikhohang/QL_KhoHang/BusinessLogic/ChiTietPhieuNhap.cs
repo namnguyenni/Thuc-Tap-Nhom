cb86c9651120b68d3630a68e9aa55a47031af657
DoVanHao <DoVanHao@users.noreply.github.com>
2019-09-25 03:05:11 -0700

    commit by hao

--- Bai4_Quanlikhohang/QL_KhoHang/BusinessLogic/ChiTietPhieuNhap.cs
+++ Bai4_Quanlikhohang/QL_KhoHang/BusinessLogic/ChiTietPhieuNhap.cs
@@ -40,37 +40,6 @@ namespace BusinessLogic
         }
 
 
-        public DataTable ShowCTPN1(string DieuKien)
-        {
-            string sql = @"SELECT MaHH, SoLuong, DonGia, ThanhTien FROM CHITIETPHIEUXUAT " + DieuKien;
-            DataTable dt = new DataTable();
-            SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
-            SqlDataAdapter ad = new SqlDataAdapter(sql, con);
-            ad.Fill(dt);
-            return dt;
-        }
-
-        public void InsertCTPN1(string mapx, string mahh, int soluong, long dongia, long thanhtien)
-        {
-            string sql = "ThemCTPX";
-            SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
-            con.Open();
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            cmd.Parameters.AddWithValue("@mapx", mapx);
-            cmd.Parameters.AddWithValue("@mahh", mahh);
-            cmd.Parameters.AddWithValue("@soluong", soluong);
-            cmd.Parameters.AddWithValue("@dongia", dongia);
-            cmd.Parameters.AddWithValue("@thanhtien", thanhtien);
-
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            con.Close();
-
-        }
-
-
         public DataTable HienThi(string DieuKien)
         {
             string sql = @"SELECT * FROM dbo.CHITIETPHIEUNHAP WHERE MaPN = '" + DieuKien + "'";
