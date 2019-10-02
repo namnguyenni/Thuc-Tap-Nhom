6659022f9b82dc1b08ba35074f714e9decd6b1c4
Nam <namnguyen.lion@gmail.com>
2019-10-02 08:32:41 -0700

    nam commit
    Co-Authored-By: nam <nam@users.noreply.github.com>

--- Bai1_QLNhanSu/BangQLCT/BUS_DonVi.cs
+++ Bai1_QLNhanSu/BangQLCT/BUS_DonVi.cs
@@ -23,28 +23,6 @@ namespace BangQLCT
             return dt;
         }
 		
-		        public DataTable HienThiDonVi()
-        {
-            string sql = "SELECT * FROM dbo.DonVi";
-            DataTable dt = new DataTable();
-            SqlConnection conn = new SqlConnection(KetNoi.connect());
-            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
-            da.Fill(dt);
-            return dt;
-        }
-		
-		        public DataTable HienThiDonVi()
-        {
-            string sql = "SELECT * FROM dbo.DonVi";
-            DataTable dt = new DataTable();
-            SqlConnection conn = new SqlConnection(KetNoi.connect());
-            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
-            da.Fill(dt);
-            return dt;
-        }
-		
-		        public DataTable HienThiDonVi()
-        {
             string sql = "SELECT * FROM dbo.DonVi";
             DataTable dt = new DataTable();
             SqlConnection conn = new SqlConnection(KetNoi.connect());
