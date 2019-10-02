d276dc7dc68d65ebe7cfdbdfb6188d916d2d7862
Nam <namnguyen.lion@gmail.com>
2019-10-02 08:35:39 -0700

    Create KetNoi_Database.cs
    nam commit
    
    Co-Authored-By: nam <nam@users.noreply.github.com>

---
+++ Bai1_QLNhanSu/KetNoiDB/KetNoi_Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace KetNoiDB
+{
+    public class KetNoi
+    {
+        SqlConnection con = new SqlConnection();
+
+        public static String connect()
+        {
+            return (@"Data Source=DESKTOP-42NHODK;Initial Catalog=QLCT_2;Integrated Security=True");
+        }
+    }
+}
