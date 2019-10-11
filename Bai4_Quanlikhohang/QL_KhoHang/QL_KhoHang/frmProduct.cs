24969b7654f81d38ce481bb395a8cb56e7d62557
Nam <namnguyen.lion@gmail.com>
2019-10-10 18:04:00 -0700

    form product

---
+++ Bai4_Quanlikhohang/QL_KhoHang/QL_KhoHang/frmProduct.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace QL_KhoHang
+{
+    public partial class frmHuongDan : Form
+    {
+        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+
+		
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+		
+		        public frmHuongDan()
+        {
+            InitializeComponent();
+        }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+        private void trViewGioiThieu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+    
+                        GetFileAll("PhanMain.txt");
+                        Image img = Image.FromFile(@"b4 anh main.png");
+                        pictureBox1.BackgroundImage = img;
+                    }
+             else if (e.Node.Name == "gtHangHoa")
+                        {
+                            GetFileAll("PhanHangHoa.txt");
+                            Image img = Image.FromFile(@"b4 hanghoa.png");
+                            pictureBox1.BackgroundImage = img;
+                        }
+            else if (e.Node.Name == "gtKhachHang")
+            {
+                GetFileAll("PhanKhachHang.txt");
+                Image img = Image.FromFile(@"b4 khachhang.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtNhaCungCap")
+            {
+                GetFileAll("PhanNhaCungCap.txt");
+                Image img = Image.FromFile(@"b4 nhacungcap.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtNhapXuat")
+            {
+                GetFileAll("PhanNhapXuat.txt");
+                Image img = Image.FromFile(@"b4 nhapxuat.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtThemPN")
+            {
+                GetFileAll("PhanThemPN.txt");
+                Image img = Image.FromFile(@"b4 thempn.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtThemPX")
+            {
+                GetFileAll("PhanThemPX.txt");
+                Image img = Image.FromFile(@"b4 thempx.png");
+                pictureBox1.BackgroundImage = img;
+            }
+        }
+    }
+}
---
+++ Bai4_Quanlikhohang/QL_KhoHang/QL_KhoHang/frmSanPhamSau.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+
+namespace QL_KhoHang
+{
+    public partial class frmHangHoa : Form
+    {
+        public frmHangHoa()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+        HangHoa hh = new HangHoa();
+        public void HienThi()
+        {
+            dataGridView1.DataSource = hh.ShowHangHoa("");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtMaSP.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtTenSP.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtSL.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtGN.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtGB.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();               
+                txtNSX.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+                txtMoTa.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+            }
+            catch { }
+        }
+        private void txttk_TenHH_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = hh.TKHH_TenHH(txttk_TenHH.Text);
+
+        }
+        private void txttk_NSX_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = hh.TKHH_NSX(txttk_NSX.Text);
+        }
+
+        private void txtGB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSL_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
